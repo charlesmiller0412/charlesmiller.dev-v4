2eba665f346ae0404c13524a795d7005cd778845
Charles Miller <charles.miller.0412@gmail.com>
2022-08-19 11:41:50 -0500

    updated links

--- form-to-email.php
+++ form-to-email.php
@@ -17,33 +17,13 @@
   $to = 'charles@charlesmiller.dev';
   $headers = 'From: ' . $visitor_email;
 
-  $secretKey = "6LfIeVAgAAAAANIXD42CAPVLUTNBG19OJ9pB3rqY";
-  $responseKey = $_POST['g-recaptcha-response'];
-  $UserIP = $_SERVER['REMOTE_ADDR'];
-  $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$responseKey&remoteip=$UserIP";
-
-  $response = file_get_contents($url);
-  $response = json.decode($response);
-
-  if ($response->success) {
-    mail($to, $email_subject, $email_body, $headers);
-    echo "yay";
-  } else {
-    echo "Invalid Captcha, Please Try Again";
-  }
-
-
   //Send email
   mail($to, $email_subject, $email_body, $headers);
   if (mail){
-    //You need to redirect
-    // echo 'Thanks! I\'ll get back to you ASAP!';
-    header('Location: http://www.charlesmiller.dev/thank_you.html');
+    echo 'Message sent successfully';
   }
   else{
     echo 'ERROR! Email was not sent';
   }
-  } else {
-    echo "That was bad";
 }
 ?>
